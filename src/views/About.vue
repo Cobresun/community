e8fcaa985220ed64ce298d44803d1360129c29b7
Brian Norman <briankn8@gmail.com>
2021-01-07 10:55:11 -0700

    Cite source and update About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,9 +2,20 @@
     <div id="about">
         <h1>About</h1>
         <div class="contentText">
-            <p>Oh boy does community have a lot of refrences.</p>
-            <p>Super-charge your next Community rewatch with every reference from every episode of our favourite NBC sitcom!</p>
-            <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</p>
+            <blockquote>"Community will likely always be a cult classic for those who can catch even a fraction of its references," - <a href="https://www.denofgeek.com/tv/community-series-review-and-appreciation/">David Crow, 2013</a></blockquote>
+
+            <blockquote>"Intoxicated with all things pop-cultural and self-referential, this smartly smart-alecky show long ago moved beyond its seemingly simple premise of Cheers in a community-college study group." - <a href="https://www.tvguide.com/news/roush-review-community-1026571/">Matt Roush, 2010</a></blockquote>
+
+            <blockquote>"That's as self-referential as you can get from a show built on pop culture-driven meta humor," - <a href="https://www.nbclosangeles.com/news/national-international/a-new-community-timeline-joel-mchale-dan-harmon-donald-glover/2054985/">Jere Hester, 2013</a></blockquote>
+
+            <p>Oh boy does community have a lot of refrences. Super-charge your next Community rewatch (or first-time watch for the lucky few who get to watch it for the first time) with every reference from every episode of our favourite NBC sitcom! We included movies/shows/books/plays/songs either referenced or parodied in each episode, without including any spoilers for the episodes themselves.</p>
+
+            <p>Most of the references were sourced from the <a href="https://community-sitcom.fandom.com/wiki/Community_Wiki">fandom's Wiki</a>, where you can also find spoiler-filled explanations of each reference/parody, including when and how they appear in the episodes.</p>
+
+            <p>Chances are we probably missed a couple (there are 110 episodes). If you know of a reference or parody we missed, please let us know so we can add it! Thanks 😃</p>
+
+            <b>#SixSeasonsAndAMovie</b>
+
         </div>
     </div>
 </template>
