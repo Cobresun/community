9095f67bdcfcb3f140fafc7efadd8c05001fff05
wesley-tang <wesleytang1@gmail.com>
2021-01-10 21:37:18 -0700

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,13 @@
     <div id="about">
         <h1>About</h1>
         <div class="contentText">
-            <blockquote>"Community will likely always be a cult classic for those who can catch even a fraction of its references," - <a href="https://www.denofgeek.com/tv/community-series-review-and-appreciation/">David Crow, 2013</a></blockquote>
+            <blockquote>"<em>Community will likely always be a cult classic for those who can catch even a fraction of its references,</em>" - <a href="https://www.denofgeek.com/tv/community-series-review-and-appreciation/">David Crow, 2013</a></blockquote>
 
-            <blockquote>"Intoxicated with all things pop-cultural and self-referential, this smartly smart-alecky show long ago moved beyond its seemingly simple premise of Cheers in a community-college study group." - <a href="https://www.tvguide.com/news/roush-review-community-1026571/">Matt Roush, 2010</a></blockquote>
+            <blockquote>"<em>Intoxicated with all things pop-cultural and self-referential, this smartly smart-alecky show long ago moved beyond its seemingly simple premise of Cheers in a community-college study group.</em>" - <a href="https://www.tvguide.com/news/roush-review-community-1026571/">Matt Roush, 2010</a></blockquote>
 
-            <blockquote>"That's as self-referential as you can get from a show built on pop culture-driven meta humor," - <a href="https://www.nbclosangeles.com/news/national-international/a-new-community-timeline-joel-mchale-dan-harmon-donald-glover/2054985/">Jere Hester, 2013</a></blockquote>
+            <blockquote>"<em>That's as self-referential as you can get from a show built on pop culture-driven meta humor,</em>" - <a href="https://www.nbclosangeles.com/news/national-international/a-new-community-timeline-joel-mchale-dan-harmon-donald-glover/2054985/">Jere Hester, 2013</a></blockquote>
 
-            <p>Oh boy does community have a lot of refrences. Super-charge your next Community rewatch (or first-time watch) with every reference from every episode of our favourite NBC sitcom! We included movies, shows, books, plays, and songs either referenced or parodied in each episode, without including any spoilers for the episodes themselves.</p>
+            <p>Oh boy does Community have a lot of references. Super-charge your next Community rewatch (or first-time watch) with every reference from every episode of our favourite NBC sitcom! We included movies, shows, books, plays, and songs either referenced or parodied in each episode, without including any spoilers for the episodes themselves.</p>
 
             <p>Most of the references were sourced from the <a href="https://community-sitcom.fandom.com/wiki/Community_Wiki">fandom's Wiki</a>, where you can find spoiler-filled explanations of each reference/parody, including when and how they appear in the episodes.</p>
 
@@ -16,6 +16,10 @@
 
             <b>#SixSeasonsAndAMovie</b>
 
+            <br>
+            <br>
+
+            <p style="font-size:0.8em;">We are not affiliated with, nor endorsed by NBC or Community. All images are property of their respective owners and are sourced from the <a href="https://community-sitcom.fandom.com/wiki/Community_Wiki">fandom's Wiki</a>.</p>
         </div>
     </div>
 </template>
