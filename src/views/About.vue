77650142caa85a0738bfe9e536a258140ddacae4
cole-adams <cole_adams@shaw.ca>
2021-01-13 10:44:49 -0700

    Add contributor list

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,7 +10,7 @@
 
             <p>Oh boy does Community have a lot of references. Super-charge your next Community rewatch (or first-time watch) with every reference from every episode of our favourite NBC sitcom! We included movies, shows, books, plays, and songs either referenced or parodied in each episode, without including any spoilers for the episodes themselves.</p>
 
-            <p>Most of the references were sourced from the <a href="https://community-sitcom.fandom.com/wiki/Community_Wiki">fandom's Wiki</a>, where you can find spoiler-filled explanations of each reference/parody, including when and how they appear in the episodes.</p>
+            <p>Most of the references were sourced from the <a href="https://community-sitcom.fandom.com/wiki/Community_Wiki">fandom's Wiki</a>, where you can find spoiler-filled explanations of each reference/parody, including when and how they appear in the episodes. See our full list of reference sources <router-link to="/contributors">here</router-link>.</p>
 
             <p>Chances are, we probably missed a couple (there are 110 episodes). If you know of a reference or parody we missed, please let us know so we can add it! Thanks 😃</p>
 
---
+++ src/views/Contributors.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="contributors">
+        <h1>Reference Contributors</h1>
+        <ul class="content-text">
+          <li><a href="https://community-sitcom.fandom.com/wiki/Community_Wiki">Community Fandom Wiki</a></li>
+          <li><a href="https://www.reddit.com/r/community/comments/kuuwck/check_out_this_searchable_index_of_every/gj2426t?utm_source=share&utm_medium=web2x&context=3">
+            thecoma3</a> pointed out <router-link to="/ep/106">Season 6 Episode 10</router-link> references Breaking Bad</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contributors'
+}
+</script>
+
+<style scoped>
+#contributors {
+    padding-top: 12rem;
+}
+
+.content-text{
+    text-align: left;
+    width: 50%;
+    margin: auto;
+}
+
+@media only screen and (max-width: 750px) {
+    #contributors {
+        padding-top: 9rem;
+    }
+    .content-text {
+        width: 95%;
+    }
+}
+</style>
